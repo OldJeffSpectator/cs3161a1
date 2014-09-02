b20beb788b76dae7128b18620ac64dcf25794a51
EAPierzchalski <e.a.pierzchalski@gmail.com>
2014-09-02 14:45:07 +1000

    Add some more eval instances

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -25,14 +25,29 @@ evaluate bs = evalE E.empty (Let bs (Var "main"))
 
 
 evalE :: VEnv -> Exp -> Value
-evalE g (Var i) = case E.lookup g i of
-  Just v  ->  v
-  Nothing ->  error $ "Variable " ++ show i ++ " not in scope"
+evalE env (Var id) = case E.lookup env id of
+  Just v -> v
+  Nothing -> error $ "Variable " ++ id ++ " not in scope."
+
+evalE _ (Con id) = case id of
+  "True" -> B True
+  "False" -> B False
+  _ -> error $ "Unknown constant \"" ++ id ++ "\"."
+
 evalE _ (Num n) = I n
-evalE _ (Con i) = case i of
-  "true" -> B True
-  "false" -> B False
-evalE g (If p t e) = case evalE g p of
-  B True -> evalE g t
-  B False -> evalE g e
-  _ -> error $ "Expression " ++ show p ++ " did not eval to a bool"
+
+evalE env (If p t e) = case evalE env p of
+  B True -> evalE env t
+  B False -> evalE env e
+  _ -> error $ "Expression " 
+    ++ show p ++ " does not eval to a bool."
+
+evalE env (Let [bind] body) = case bind of
+  Bind id _ _ def -> 
+    let env' = E.add env (id, evalE env def) in
+      evalE env' body
+
+evalE _ e = 
+  let msg = PP.renderPretty 1.0 80 $ PP.pretty e in
+    error $  "Unimplemented for:\n" ++
+      PP.displayS msg ""
