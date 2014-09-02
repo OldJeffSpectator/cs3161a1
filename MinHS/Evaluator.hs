61332600f46f2ccd95cb39d8c6bdb964b332028f
EAPierzchalski <e.a.pierzchalski@gmail.com>
2014-09-02 20:58:42 +1000

    Add working instances

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -6,12 +6,18 @@ import qualified Text.PrettyPrint.ANSI.Leijen as PP
 
 type VEnv = E.Env Value
 
+data VFun = VFun (Value -> Value)
+
+instance Show VFun where
+  show _ = "VFun ( ??? )"
+
 data Value = I Integer
            | B Bool
            | Nil
            | Cons Integer Value
-           | Cloj VEnv [Id] Expr
            -- Others as needed
+           | Cloj VEnv Id [Id] Exp
+           | Lam VFun
            deriving (Show)
 
 instance PP.Pretty Value where
@@ -32,6 +38,7 @@ evalE env (Var id) = case E.lookup env id of
 evalE _ (Con id) = case id of
   "True" -> B True
   "False" -> B False
+  "Nil" -> Nil
   _ -> error $ "Unknown constant \"" ++ id ++ "\"."
 evalE _ (Num n) = I n
 evalE env (If p t e) = case evalE env p of
@@ -39,20 +46,88 @@ evalE env (If p t e) = case evalE env p of
   B False -> evalE env e
   _ -> error $ "Expression " 
     ++ show p ++ " does not eval to a bool."
-evalE env 
-evalE env (Let binds body) = evalLet env binds body
+evalE env (App (App (Con "Cons") e1) e2) =
+  let
+    I head = evalE env e1
+    tail = evalE env e2
+  in Cons head tail
+evalE env (App (Prim op) e) =
+  let v = evalE env e 
+  in evalUnaryOp op v
+evalE env (App (App (Prim op) e1) e2) = 
+  let
+    v1 = evalE env e1
+    v2 = evalE env e2
+  in evalBinaryOp op v1 v2
+evalE env (App e1 e2) = 
+  let 
+    f = evalE env e1
+    x = evalE env e2
+  in case f of
+    Cloj fEnv name [arg] body ->
+      let fEnv' = E.addAll fEnv [(name, f), (arg, x)]
+      in evalE fEnv' body
+    Lam (VFun g) -> g x
+    _ -> error $ "Not a valid app term " ++ show e1
+evalE env (Let binds body) = 
+  evalLet env binds body
+evalE env (Letfun (Bind name _ args body)) =
+  evalLetFun env name args body
 evalE _ e = 
   let msg = PP.renderPretty 1.0 80 $ PP.pretty e in
     error $  "Unimplemented for:\n" ++
       PP.displayS msg ""
 
+evalLetFun :: VEnv -> Id -> [Id] -> Exp -> Value
+evalLetFun env name [] body = 
+  let env' = E.add env (name, evalLetFun env name [] body)
+  in evalE env' body
+evalLetFun env name args body =
+  Cloj env name args body
+
+evalUnaryOp :: Op -> Value -> Value
+evalUnaryOp Neg (I n) = I (-n)
+evalUnaryOp Null l = case l of
+  Nil -> B True
+  Cons _ _ -> B False
+evalUnaryOp Head l = case l of
+  Nil -> error $ "Head of nil"
+  Cons i _ -> I i
+  _ -> error $ "What? " ++ show l
+evalUnaryOp Tail l = case l of
+  Nil -> error $ "Tail of nil"
+  Cons _ t -> t
+evalUnaryOp op v = error $ "-- No unary op for op:\n"
+  ++ show op
+  ++ "\n-- value:\n"
+  ++ show v
+
+evalBinaryOp :: Op -> Value -> Value -> Value
+evalBinaryOp op (I n1) (I n2) = case op of
+  Add -> I (n1 + n2)
+  Sub -> I (n1 - n2)
+  Mul -> I (n1 * n2)
+  Quot -> I (n1 `div` n2)
+  Rem -> I (n1 `rem` n2)
+  Eq -> B (n1 == n2)
+  Gt -> B (n1 > n2)
+  Ge -> B (n1 >= n2)
+  Lt -> B (n1 < n2)
+  Le -> B (n1 <= n2)
+  Ne -> B (n1 /= n2)
+  other -> error $ "No binary op for " ++ show other ++ "."
+evalBinaryOp op l r = error $ "-- No binary op for op\n" 
+  ++ show op
+  ++ "\n-- left hand side\n"
+  ++ show l
+  ++ "\n-- right hand side\n"
+  ++ show r
+
 evalLet :: VEnv -> [Bind] -> Exp -> Value
 evalLet _ [] _ = error $ "Empty bind list"
-evalLet env (bind : []) body = case bind of
-  Bind id _ args def ->
-    let env' = E.add env (id, evalE env def)
-    in evalE env' body
 evalLet env (bind : binds) body = case bind of
   Bind id _ args def ->
     let env' = E.add env (id, evalE env def)
-    in evalLet env' binds body
+    in case binds of 
+      [] -> evalE env' body
+      _  -> evalLet env' binds body
