70bb328a0accc79b9ac65b881b4642d0d7b1b949
EAPierzchalski <e.a.pierzchalski@gmail.com>
2014-09-11 17:54:19 +1000

    Clean up avarthang

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -30,50 +30,33 @@ evaluate :: Program -> Value
 evaluate bs = evalE E.empty (Let bs (Var "main"))
 
 evalE :: VEnv -> Exp -> Value
-
-evalE env (Var id) = case E.lookup env id of
+evalE env (Var name) = case E.lookup env name of
   Just v -> v
-  Nothing -> error $ "Variable " ++ id ++ " not in scope."
-
-evalE _ (Con id) = case id of
+  _      -> error $ "Not in scope: " ++ name
+evalE _   (Prim op) = evalOp op
+evalE _   (Con val) = case val of
   "True" -> B True
   "False" -> B False
   "Nil" -> Nil
   "Cons" -> 
-    lam $ \(I head) ->
-      lam $ \tail ->
-        Cons head tail
-  _ -> error $ "Unknown constant \"" ++ id ++ "\"."
-
-evalE _ (Num n) = I n
-
+    lam $ \(I h) ->
+      lam $ \t ->
+        Cons h t
+evalE _   (Num n) = I n
 evalE env (If p t e) = case evalE env p of
   B True -> evalE env t
   B False -> evalE env e
-  _ -> error $ "Expression " 
-    ++ show p ++ " does not eval to a bool."
-
-evalE env (Prim op) = evalOp op
-
 evalE env (App e1 e2) = 
-  let 
-    f = evalE env e1
+  let
+    Lam (VFun f) = evalE env e1
     x = evalE env e2
-  in evalApp f x
-
+  in f x
 evalE env (Let binds body) = 
   evalLet env binds body
-
 evalE env (Letfun (Bind name _ args body)) =
   evalLetFun env name args body
-
 evalE env (Letrec binds body) = 
   evalLetRec env binds body
-
-evalE _ e = 
-  let msg = PP.renderPretty 1.0 80 $ PP.pretty e in
-    error $  "Unimplemented for:\n" ++
-      PP.displayS msg ""
 
 lam :: (Value -> Value) -> Value
 lam f = Lam $ VFun f
@@ -94,24 +77,16 @@ evalLet env (bind : binds) body = case bind of
 
 evalLetFun :: VEnv -> Id -> [Id] -> Exp -> Value
 evalLetFun env name args body =
-  -- recursive call binds the knot,
-  -- distinguishes letFun from let
   let env' = E.add env (name, evalLetFun env name args body)
   in bindLam env' args body
 
 evalLetRec :: VEnv -> [Bind] -> Exp -> Value
 evalLetRec env binds body =
-  -- using mutual recursion to implement mutual recursion?
-  -- cheaty! 
   let
-    eval (Bind name _ args body) = (name, bindLam env' args body)
+    eval (Bind name _ args def) = (name, bindLam env' args def)
     bindVals = eval `map` binds
     env' = E.addAll env bindVals
   in evalE env' body
-
-evalApp :: Value -> Value -> Value  
-evalApp (Lam (VFun g)) x = g x
-evalApp f _ = error $ "Not a valid app term " ++ show f
 
 evalOp :: Op -> Value
 evalOp op = 
@@ -123,24 +98,24 @@ evalOp op =
       lam $ \(I n1) ->
         lam $ \(I n2) -> B (n1 `f` n2)
   in case op of
-    Add -> intOp (+)
-    Sub -> intOp (-)
-    Mul -> intOp (*)
+    Add  -> intOp (+)
+    Sub  -> intOp (-)
+    Mul  -> intOp (*)
     Quot -> intOp div
-    Rem -> intOp rem
-    Eq -> boolOp (==)
-    Gt -> boolOp (>)
-    Ge -> boolOp (>=)
-    Lt -> boolOp (<)
-    Le -> boolOp (<=)
-    Ne -> boolOp (/=)
-    Neg -> lam $ \(I n) -> I (-n)
+    Rem  -> intOp rem
+    Eq   -> boolOp (==)
+    Gt   -> boolOp (>)
+    Ge   -> boolOp (>=)
+    Lt   -> boolOp (<)
+    Le   -> boolOp (<=)
+    Ne   -> boolOp (/=)
+    Neg  -> lam $ \(I n) -> I (-n)
     Null -> lam $ \l -> case l of
-      Nil -> B True
       Cons _ _ -> B False
+      Nil      -> B True
     Head -> lam $ \l -> case l of
-      Nil -> error $ "Head of nil!"
       Cons i _ -> I i
+      _        -> error $ "Head of Nil!"
     Tail -> lam $ \l -> case l of
-      Nil -> error $ "Tail of nil!"
       Cons _ t -> t
+      _        -> error $ "Tail of Nil!"
